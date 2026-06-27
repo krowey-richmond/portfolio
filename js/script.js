d9c4604c0625d61abb8caf4e61d7fdf592489192
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2026-06-27 19:29:16 +0000

    refactor : add js code for navToggle icon

--- js/script.js
+++ js/script.js
@@ -4,10 +4,27 @@ const navMenu = document.querySelector(".nav-links");
 const mainNav = document.querySelector(".main-nav");
 const mobileQuery = window.matchMedia("(max-width: 768px)");
 
+const navItems = document.querySelectorAll(".nav-links a");
+
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("open-menu");
 });
 
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.classList.remove("open-menu");
+  });
+});
+
+document.addEventListener("click", (event) => {
+  if (!navMenu.contains(event.target) && !navToggle.contains(event.target)) {
+    navMenu.classList.remove("open-menu");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  navMenu.classList.remove("open-menu");
+});
 function updateNavLabel() {
   mainNav.setAttribute(
     "aria-label",
