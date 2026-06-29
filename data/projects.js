5e2055b8610c3af3c9a0fc59b0cbcc7dc38a721c
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2026-06-29 03:44:57 +0000

    add grocery shop project

--- data/projects.js
+++ data/projects.js
@@ -1,12 +1,13 @@
 export const projects = [
 	{
 		label: 1,
-		title: "Portfolio",
-		image: "./assets/images/portfolio.webp",
-		description: "My portfolio",
-		tech: ["HTML", "CSS"],
-		live: "#",
-		code: "#",
+		title: "Grocery Shop",
+		image: "./assets/images/grocery.webp",
+		description:
+			"A responsive grocery e-commerce landing page built with HTML, CSS, and JavaScript",
+		tech: ["HTML", "CSS", "JavaScript"],
+		live: "https://grocery-shop-krb.vercel.app/",
+		code: "https://github.com/krowey-richmond/grocery-shop",
 	},
 	{
 		label: 2,
